1547acf3b20ed65ac49c57a3254c1c172bdf04e1
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-03-30 02:47:54 -0600

    Fix little bug

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git clone https://github.com/omireyes19/dpa-metro-cdmx.git
-cd dpa_metro_cdmx/
 pyenv virtualenv 3.7.4 dpa_metro_cdmx
 echo 'dpa_metro_cdmx' > .python-version
 pip install -f requirements.txt
--- pyenv_installment.sh
+++ pyenv_installment.sh
@@ -6,3 +6,5 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 exec "$SHELL"
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+git clone https://github.com/omireyes19/dpa-metro-cdmx.git
+cd dpa_metro_cdmx/
