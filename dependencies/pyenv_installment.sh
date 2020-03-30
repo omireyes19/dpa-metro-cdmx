fc910176bba37a0d96750074acc0f2ad8eb4e539
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-03-30 01:04:02 -0600

    Update pyenv_installments file

--- dependencies/pyenv_installment.sh
+++ dependencies/pyenv_installment.sh
@@ -6,7 +6,6 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 exec "$SHELL"
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-pyenv install 3.7.4
 pyenv virtualenv 3.7.4 dpa_metro_cdmx
 echo 'dpa_metro_cdmx' > .python-version
 pip install -f requirements.txt
