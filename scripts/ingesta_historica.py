38233d1f92a6c1e572025cf4bc2229d655585645
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-03-31 14:54:29 -0600

    Arreglar identación

--- scripts/ingesta_historica.py
+++ scripts/ingesta_historica.py
@@ -9,27 +9,8 @@ import os
 
 class data_acq_task(luigi.Task):
     bucket = 'dpa-metro'
-    years = ['2010', '2011', '2012', '2013', '2014',
- 			 '2015', '2016', '2017', '2018', '2019']
-	stations = ['Pantitlán', 'Chabacano', 'Tacubaya', 'Atlalilco', 'Balderas', 'Bellas Artes',
-				'Candelaria', 'Centro Médico', 'Consulado', 'Deptvo. 18 de Marzo', 'El Rosario', 
-				'Ermita', 'Garibaldi', 'Guerrero', 'Hidalgo', 'Inst. del Petróleo', 'Jamaica', 
-				'La Raza', 'Martín Carrera', 'Mixcoac', 'Morelos', 'Oceanía', 'Pino Suárez', 
-				'Salto del Agua', 'San Lázaro', 'Santa Anita', 'Tacuba', 'Zapata', '20 de Noviembre',
-				'Acatitla', 'Aculco', 'Agrícola Oriental', 'Allende', 'Apatlaco', 'Aquiles Serdán',
-				'Aragón', 'Auditorio', 'Autobuses del Norte', 'Azcapotzalco', 'Balbuena', 
-				'Barranca del Muerto', 'Blvd. Puerto Aéreo', 'Bondojito', 'Bosque de Aragón',
-				'Buenavista', 'Calle 11', 'Camarones', 'Canal de San Jua', 'Canal del Norte',
-				'Cerro de la Estrella', 'Chapultepec', 'Chilpancingo', 'Ciudad Azteca', 
-				'Ciudad Deportiva', 'Colegio Militar', 'Constitución de 1917', 'Constituyentes',
-				'Copilco', 'Coyoacán', 'Coyuya', 'Cuatro Caminos', 'Cuauhtémoc', 'Cuitláhuac',
-				'Culhuacán', 'Deportivo Oceanía', 'División del Norte', 'Doctores', 'Eduardo Molina',
-				'Eje Central', 'Escuadrón 201', 'Etiopía', 'Eugenia', 'Ferrería', 'Fray Servando', 
-				'General Anaya', 'Guelatao', 'Gómez Farías', 'Hangares', 'Hospital General', 'Impulsora',
-				'Indios Verdes', 'Insurgentes', 'Insurgentes Sur', 'Isabel la Católica', 'Iztacalco', 
-				'Iztapalapa', 'Juanacatlán', 'Juárez', 'La Paz', 'La Viga', 'La Villa-Basilica', 
-				'Lagunilla', 'Lindavista', 'Lomas Estrella', 'Los Reyes', 'Lázaro Cárdenas', 'Merced',
-				'Mexicaltzingo', 'Miguel A. de Q.', 'Misterios']
+    years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019']
+	stations = ['Pantitlán', 'Chabacano', 'Tacubaya', 'Atlalilco', 'Balderas', 'Bellas Artes','Candelaria', 'Centro Médico', 'Consulado', 'Deptvo. 18 de Marzo', 'El Rosario', 'Ermita', 'Garibaldi', 'Guerrero', 'Hidalgo', 'Inst. del Petróleo', 'Jamaica', 'La Raza', 'Martín Carrera', 'Mixcoac', 'Morelos', 'Oceanía', 'Pino Suárez', 'Salto del Agua', 'San Lázaro', 'Santa Anita', 'Tacuba', 'Zapata', '20 de Noviembre','Acatitla', 'Aculco', 'Agrícola Oriental', 'Allende', 'Apatlaco', 'Aquiles Serdán','Aragón', 'Auditorio', 'Autobuses del Norte', 'Azcapotzalco', 'Balbuena', 'Barranca del Muerto', 'Blvd. Puerto Aéreo', 'Bondojito', 'Bosque de Aragón','Buenavista', 'Calle 11', 'Camarones', 'Canal de San Jua', 'Canal del Norte','Cerro de la Estrella', 'Chapultepec', 'Chilpancingo', 'Ciudad Azteca', 'Ciudad Deportiva', 'Colegio Militar', 'Constitución de 1917', 'Constituyentes','Copilco', 'Coyoacán', 'Coyuya', 'Cuatro Caminos', 'Cuauhtémoc', 'Cuitláhuac','Culhuacán', 'Deportivo Oceanía', 'División del Norte', 'Doctores', 'Eduardo Molina','Eje Central', 'Escuadrón 201', 'Etiopía', 'Eugenia', 'Ferrería', 'Fray Servando', 'General Anaya', 'Guelatao', 'Gómez Farías', 'Hangares', 'Hospital General', 'Impulsora','Indios Verdes', 'Insurgentes', 'Insurgentes Sur', 'Isabel la Católica', 'Iztacalco', 'Iztapalapa', 'Juanacatlán', 'Juárez', 'La Paz', 'La Viga', 'La Villa-Basilica', 'Lagunilla', 'Lindavista', 'Lomas Estrella', 'Los Reyes', 'Lázaro Cárdenas', 'Merced','Mexicaltzingo', 'Miguel A. de Q.', 'Misterios']
 
 	for year in years:
   		for station in stations:
