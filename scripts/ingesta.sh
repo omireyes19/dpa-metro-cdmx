82fda19b4ab329ac59cd6df5b46ab1676f8eacbe
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-03-31 18:05:45 -0600

    Agregamos script para carga productiva

---
+++ scripts/ingesta.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Pantitlán --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Chabacano --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Tacubaya --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Atlalilco --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Balderas --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Bellas\ Artes --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Candelaria --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Centro\ Médico --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Consulado --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Deptvo.\ 18\ de\ Marzo --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station El\ Rosario --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Ermita --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Garibaldi --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Guerrero --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Hidalgo --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Inst.\ del\ Petróleo --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Jamaica --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station La\ Raza --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Martín\ Carrera --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Mixcoac --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Morelos --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Oceanía --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Pino\ Suárez --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Salto\ del\ Agua --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station San\ Lázaro --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Santa\ Anita --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Tacuba --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Zapata --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station 20\ de\ Noviembre --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Acatitla --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Aculco --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Agrícola\ Oriental --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Allende --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Apatlaco --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Aquiles\ Serdán --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Aragón --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Auditorio --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Autobuses\ del\ Norte --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Azcapotzalco --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Balbuena --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Barranca\ del\ Muerto --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Blvd.\ Puerto\ Aéreo --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Bondojito --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Bosque\ de\ Aragón --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Buenavista --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Calle\ 11 --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Camarones --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Canal\ de\ San\ Jua --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Canal\ del\ Norte --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Cerro\ de\ la\ Estrella --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Chapultepec --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Chilpancingo --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Ciudad\ Azteca --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Ciudad\ Deportiva --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Colegio\ Militar --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Constitución\ de\ 1917 --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Constituyentes --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Copilco --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Coyoacán --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Coyuya --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Cuatro\ Caminos --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Cuauhtémoc --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Cuitláhuac --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Culhuacán --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Deportivo\ Oceanía --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station División\ del\ Norte --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Doctores --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Eduardo\ Molina --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Eje\ Central --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Escuadrón\ 201 --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Etiopía --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Eugenia --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Ferrería --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Fray\ Servando --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station General\ Anaya --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Guelatao --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Gómez\ Farías --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Hangares --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Hospital\ General --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Impulsora --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Indios\ Verdes --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Insurgentes --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Insurgentes\ Sur --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Isabel\ la\ Católica --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Iztacalco --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Iztapalapa --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Juanacatlán --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Juárez --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station La\ Paz --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station La\ Viga --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station La\ Villa-Basilica --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Lagunilla --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Lindavista --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Lomas\ Estrella --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Los\ Reyes --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Lázaro\ Cárdenas --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Merced --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Mexicaltzingo --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Miguel\ A.\ de\ Q. --local-scheduler
+PYTHONPATH='.' AWS_PROFILE=omar luigi --module ingesta_productiva data_acq_task --year 2020 --station Misterios --local-scheduler
