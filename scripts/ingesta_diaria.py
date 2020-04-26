d1a7f7d004e8beb55ccf0038885472123b7a283e
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-04-25 23:12:49 -0500

    arreglar bug para ingesta diaria

--- scripts/ingesta_diaria.py
+++ scripts/ingesta_diaria.py
@@ -27,7 +27,7 @@ class data_acq_task(luigi.Task):
 		records = []
 		for day in range(days_in_month):
 			fecha = str(self.year)+"-"+str(self.month).zfill(2)+"-"+str(day+1).zfill(2)
-			api_url = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=afluencia-diaria-del-metro-cdmx&sort=-fecha&facet=fecha&facet=linea&facet=estacion&refine.fecha="+fecha+"&refine.estacion="+station
+			api_url = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=afluencia-diaria-del-metro-cdmx&sort=-fecha&facet=fecha&facet=linea&facet=estacion&refine.fecha="+fecha+"&refine.estacion="+self.station
 
 			r = requests.get(url = api_url)
 			data = r.json()
