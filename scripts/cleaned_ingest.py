a291f087a381fb6d7af0e368eb76247882555eab
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-04-26 12:34:26 -0500

    arreglar un bug para ingesta cleaned

--- scripts/cleaned_ingest.py
+++ scripts/cleaned_ingest.py
@@ -34,9 +34,10 @@ class cleaned_task(luigi.Task):
 		cal = Mexico()
 
 		df["date"] = pd.to_datetime(df['date'])
+		
 		df["day_of_week"] = df["date"].dt.dayofweek
-
 		df['holiday'] = df.date.apply(lambda x: cal.is_working_day(x))
+		df['line_crossing']= df.date.map(df.date.value_counts()-1)
 
 		with self.output().open('w') as output_file:
 			df.to_csv(output_file)
