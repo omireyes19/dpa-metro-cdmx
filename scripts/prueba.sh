2d6fbd815a8c236edcfea6d2fed48b47429f323b
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-04-26 13:29:59 -0500

    agregamos archivo .sh para hacer ingesta historica

---
+++ scripts/prueba.sh
@@ -0,0 +1,10 @@
+while read i
+do
+	for j in $(seq 2010 2020)
+	do
+		for k in $(seq 1 12)
+		do
+			echo "PYTHONPATH='.' AWS_PROFILE=omar luigi --module cleaned_ingest cleaned_task --year $j --month $k --station $i --local-scheduler"
+		done
+	done
+done < ./estaciones.txt
