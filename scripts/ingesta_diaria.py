757918450e7a359c6b9c328ecd9aa3f79cd181c8
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-04-25 22:56:16 -0500

    arreglar bug para ingesta diaria

--- scripts/ingesta_diaria.py
+++ scripts/ingesta_diaria.py
@@ -24,23 +24,23 @@ class data_acq_task(luigi.Task):
 
         days_in_month = monthrange(self.year, self.month)[1]
 
-        records = []
+		records = []
 		for day in range(days_in_month):
-		    fecha = str(year)+"-"+str(month).zfill(2)+"-"+str(day+1).zfill(2)
-		    api_url = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=afluencia-diaria-del-metro-cdmx&sort=-fecha&facet=fecha&facet=linea&facet=estacion&refine.fecha="+fecha+"&refine.estacion="+station
+			fecha = str(year)+"-"+str(month).zfill(2)+"-"+str(day+1).zfill(2)
+			api_url = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=afluencia-diaria-del-metro-cdmx&sort=-fecha&facet=fecha&facet=linea&facet=estacion&refine.fecha="+fecha+"&refine.estacion="+station
 
-		    r = requests.get(url = api_url)
-		    data = r.json()
-		    
-		    for obs in data["records"]:
-		        records.append(obs["fields"])
+			r = requests.get(url = api_url)
+			data = r.json()
 
-	    with self.output().open('w') as output_file:
-	        json.dump(records, output_file)
+			for obs in data["records"]:
+				records.append(obs["fields"])
 
-    def output(self):
-        output_path = "s3://{}/information_year_month={}/station={}/{}.json".format(self.bucket,self.fecha,self.station,self.fecha+self.station)
-        return luigi.contrib.s3.S3Target(path=output_path)
+		with self.output().open('w') as output_file:
+			json.dump(records, output_file)
+
+	def output(self):
+		output_path = "s3://{}/information_year_month={}/station={}/{}.json".format(self.bucket,self.fecha,self.station,self.fecha+self.station)
+		return luigi.contrib.s3.S3Target(path=output_path)
 
 class data_acq_metadata(luigi.Task):
     bucket_metadata = 'dpa-metro-metadata'
