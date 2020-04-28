1d20c0d901e7f6e0831653e058e954e845b40431
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-04-28 18:42:17 -0500

    arreglar bug

--- scripts/cleaned_ingest.py
+++ scripts/cleaned_ingest.py
@@ -48,29 +48,29 @@ class cleaned_task(luigi.Task):
 		return luigi.contrib.s3.S3Target(path=output_path)
 
 class cleaned_task_metadata(luigi.Task):
-    bucket_metadata = 'dpa-metro-metadata'
-    today = date.today().strftime("%d%m%Y")
-    year = luigi.Parameter()
-    month = luigi.Parameter()
-    station = luigi.Parameter()
+	bucket_metadata = 'dpa-metro-metadata'
+	today = date.today().strftime("%d%m%Y")
+	year = luigi.Parameter()
+	month = luigi.Parameter()
+	station = luigi.Parameter()
 
-    def requires(self):
+	def requires(self):
 		return cleaned_task(self.year,self.month,self.station)
 
-    def run(self):
-        ses = boto3.session.Session(profile_name='omar', region_name='us-east-1')
-        s3_resource = ses.resource('s3')
+	def run(self):
+		ses = boto3.session.Session(profile_name='omar', region_name='us-east-1')
+		s3_resource = ses.resource('s3')
 
-        obj = s3_resource.Bucket(self.bucket_metadata)
-        print(ses)
+		obj = s3_resource.Bucket(self.bucket_metadata)
+		print(ses)
 
-        with self.output_metadata().open('w') as output_file:
-            output_file.write(str(self.today)+","+self.year+","+self.month+","+self.station)
+		with self.output_metadata().open('w') as output_file:
+			output_file.write(str(self.today)+","+self.year+","+self.month+","+self.station)
 
-    def output_metadata(self):
-        output_path = "s3://{}/cleaned/DATE={}/{}.csv".\
-        format(self.bucket_metadata,str(self.today),str(self.today))
-        return luigi.contrib.s3.S3Target(path=output_path)
+	def output_metadata(self):
+		output_path = "s3://{}/cleaned/DATE={}/{}.csv".\
+		format(self.bucket_metadata,str(self.today),str(self.today))
+		return luigi.contrib.s3.S3Target(path=output_path)
 
 if __name__ == '__main__':
-    luigi.run()
+	luigi.run()
