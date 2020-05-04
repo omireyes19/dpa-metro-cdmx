76416404af681973e0cca141062a07114e0129af
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-04 02:40:58 -0500

    Arreglar bug para entrenamiento

--- scripts/training.py
+++ scripts/training.py
@@ -96,8 +96,8 @@ class training_task(PySparkTask):
 
 		predictions = cvModel.transform(testingData).toPandas()
 
-		with self.output()["predictions"].open('w') as predictions_file:
-			predictions.to_csv(predictions_file)
+		with self.output().open('w') as output_file:
+			predictions.to_csv(output_file)
 
 		#with self.output()["model"].open('w') as model_file:
 		#	cvModel.bestModel.save(sc, model_file)
@@ -105,10 +105,7 @@ class training_task(PySparkTask):
 	def output(self):
 		output_path = "s3://{}/year={}/month={}/station={}/{}.csv".\
 		format(self.bucket,str(self.year),str(self.month).zfill(2),self.station,self.station.replace(' ', ''))
-
-		model_path = "s3://{}/year={}/month={}/station={}/{}.sav".\
-		format(self.bucket,str(self.year),str(self.month).zfill(2),self.station,self.station.replace(' ', ''))
-		return {"predictions":luigi.contrib.s3.S3Target(path=output_path), "model":luigi.contrib.s3.S3Target(path=model_path)}
+		return luigi.contrib.s3.S3Target(path=output_path)
 
 import sys
 from pyspark import SparkContext
