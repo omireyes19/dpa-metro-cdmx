3a8cd684c5b79ca5d7df391ba54ed1877157e0f3
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-06 21:07:40 -0500

    cambiar s3 adress

--- scripts/prueba.py
+++ scripts/prueba.py
@@ -10,10 +10,10 @@ class prueba_task(PySparkTask):
     executor_memory = '3g'
 
     def input(self):
-        return S3Target("s3a://dpa-metro-label/year%3D2020/month%3D01/station%3DChabacano/Chabacano.csv")
+        return S3Target("s3a://dpa-metro-label/year=2020/month=02/station=Chabacano/Chabacano.csv")
 
     def output(self):
-        return S3Target("s3a://dpa-metro-label/year%3D2020/month%3D01/station%3DChabacano/Chabacano2.csv")
+        return S3Target("s3a://dpa-metro-label/year=2020/month=02/station=Chabacano/Chabacano2.csv")
 
     def main(self, sc):
         session = Session()
@@ -25,11 +25,7 @@ class prueba_task(PySparkTask):
         spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key", current_credentials.access_key)
         spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key", current_credentials.secret_key)
         spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.session.token", current_credentials.token)
-
-        #spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3.awsAccessKeyId", current_credentials.access_key)
-        #spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3.awsSecretAccessKey", current_credentials.secret_key)
-        #spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3.session.token", current_credentials.token)
-
+        spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3.impl", "org.apache.hadoop.fs.s3.S3FileSystem")
 
         p = spark.read.format('csv').options(header='true', inferSchema='true').load(self.input().path)
 
