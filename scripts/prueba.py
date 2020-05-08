9edde5bbc764cf8114c07a405c6aa2c4d4193cf1
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-07 20:53:39 -0500

    cambio forma de lectura

--- scripts/prueba.py
+++ scripts/prueba.py
@@ -22,10 +22,11 @@ class prueba_task(PySparkTask):
 
         spark = SparkSession.builder.master("local[1]").appName("SparkByExamples.com").getOrCreate()
 
+        spark.sparkContext.setSystemProperty("com.amazonaws.services.s3.enableV4", "true")
         spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key", current_credentials.access_key)
         spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key", current_credentials.secret_key)
         spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.session.token", current_credentials.token)
-        spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.endpoint","s3-us-east-1.amazonaws.com")
+        #spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.endpoint","s3-us-east-1.amazonaws.com")
         spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
         spark.sparkContext._jsc.hadoopConfiguration().set("com.amazonaws.services.s3a.enableV4", "true")
 
