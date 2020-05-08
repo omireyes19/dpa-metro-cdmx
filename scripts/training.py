58731851b53b631a33ae3ae88e96a3df18982077
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-07 23:51:54 -0500

    escribo modelo con pickle

--- scripts/training.py
+++ scripts/training.py
@@ -108,7 +108,7 @@ class training_task(PySparkTask):
 		output_path = "s3://{}/year={}/month={}/station={}/{}.csv".\
 		format(self.bucket,str(self.year),str(self.month).zfill(2),self.station,self.station.replace(' ', ''))
 
-		model_path = "s3://{}\year={}\month={}\station={}\{}.pkl".\
+		model_path = "s3://{}/year={}/month={}/station={}/{}.pkl".\
 		format(self.bucket_model,str(self.year),str(self.month).zfill(2),self.station,self.station.replace(' ', ''))
 		return {"predictions":luigi.contrib.s3.S3Target(path=output_path)
 				,"model":luigi.contrib.s3.S3Target(path=model_path)
