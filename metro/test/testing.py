a0f05d18c11bbb263bd9d4e020353aceeeec752b
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-09 18:16:35 -0500

    agrego ejemplo de pruebas unitarias

---
+++ metro/test/testing.py
@@ -0,0 +1,27 @@
+import luigi
+import luigi.contrib.s3
+from metro.test.ex1 import TestMarbles
+from datetime import date
+
+class testing(luigi.Task):
+    bucket_metadata = 'dpa-metro-metadata'
+    today = date.today().strftime("%d%m%Y")
+    year = luigi.IntParameter()
+    month = luigi.IntParameter()
+    station = luigi.Parameter()
+
+    def run(self):
+        TestMarbles.test_upper_w_marbles()
+        TestMarbles.test_isupper_w_marbles()
+        TestMarbles.test_split_w_marbles()
+
+        with self.output().open('w') as output_file:
+            output_file.write(str(self.today)+","+str(self.year)+","+str(self.month)+","+self.station)
+
+    def output(self):
+        output_path = "s3://{}/training/DATE={}/{}.csv".format(self.bucket_metadata,str(self.today),str(self.today))
+        return luigi.contrib.s3.S3Target(path=output_path)
+
+if __name__ == "__main__":
+    luigi.run()
+
