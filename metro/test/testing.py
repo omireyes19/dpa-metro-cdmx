96c067345788cdd465c56189f2b9835b07dfe44e
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-09 19:43:17 -0500

    Arreglo unit test

--- metro/test/testing.py
+++ metro/test/testing.py
@@ -1,27 +1,21 @@
+from ex1 import TestMarbles
 import luigi
 import luigi.contrib.s3
-from ex1 import TestMarbles
-from datetime import date
 
 class testing_task(luigi.Task):
-    bucket_metadata = 'dpa-metro-metadata'
-    today = date.today().strftime("%d%m%Y")
+    bucket = 'dpa-metro-metadata'
     year = luigi.IntParameter()
     month = luigi.IntParameter()
     station = luigi.Parameter()
 
     def run(self):
-        #TestMarbles.test_upper_w_marbles()
-        #TestMarbles.test_isupper_w_marbles()
-        #TestMarbles.test_split_w_marbles()
-
         with self.output().open('w') as output_file:
-            output_file.write(str(self.today)+","+str(self.year)+","+str(self.month)+","+self.station)
+            output_file.write(str(self.year)+","+str(self.month)+","+self.station)
 
     def output(self):
-        output_path = "s3://{}/unittest_prueba/DATE={}/{}.csv".format(self.bucket_metadata,str(self.today),str(self.today))
+        output_path = "s3://{}/year={}/month={}/station={}/{}.json". \
+            format(self.bucket,str(self.year),str(self.month).zfill(2),self.station,self.station.replace(' ', ''))
         return luigi.contrib.s3.S3Target(path=output_path)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     luigi.run()
-
