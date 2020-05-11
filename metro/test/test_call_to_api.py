5f81f7de04613c98f2e002cc7aaef2c2d5983883
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-10 19:05:10 -0500

    prueba unitaria raw

---
+++ metro/test/test_call_to_api.py
@@ -0,0 +1,7 @@
+import marbles.core
+from metro.ingest.call_to_api import call_to_api
+
+class TestMarbles(marbles.core.TestCase):
+    def test_records_not_empty(self,year,month,station):
+        records = call_to_api.get_information(year,month,station)
+        self.assertNotEqual(len(records), 0, note=" the names should be uppercase because bla bla bla")
