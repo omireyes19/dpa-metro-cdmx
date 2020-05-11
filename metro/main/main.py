4edb643e8ff355a226b341d4caa3f79b16fefe15
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-10 22:30:23 -0500

    corrijo main.py

--- metro/main/main.py
+++ metro/main/main.py
@@ -1,4 +1,6 @@
 import unittest
+from ParametrizedTestCase import ParametrizedTestCase
+
 suite = unittest.TestSuite()
 suite.addTest(ParametrizedTestCase.parametrize(TestOne, param=42))
 suite.addTest(ParametrizedTestCase.parametrize(TestOne, param=13))
