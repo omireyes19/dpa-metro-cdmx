0c9aa8bf926c7a07cdee5cc9b855fd852d250089
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-10 19:58:29 -0500

    prueba

--- metro/ingest/test_call_to_api.py
+++ metro/ingest/test_call_to_api.py
@@ -6,5 +6,5 @@ class TestMarbles(marbles.core.TestCase):
 
     def test_records_not_empty(self):
         #print(self.year)
-        records = call_to_api.get_information("2020","01","Chabacano")
+        records = call_to_api.get_information(self,"2020","01","Chabacano")
         self.assertNotEqual(len(records), 0, note=" the names should be uppercase because bla bla bla")
