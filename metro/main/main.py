7f5b8966f814e6132e5e698fa9c79128892d1ffe
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-10 22:28:34 -0500

    agregar ejemplo

---
+++ metro/main/main.py
@@ -0,0 +1,5 @@
+import unittest
+suite = unittest.TestSuite()
+suite.addTest(ParametrizedTestCase.parametrize(TestOne, param=42))
+suite.addTest(ParametrizedTestCase.parametrize(TestOne, param=13))
+unittest.TextTestRunner(verbosity=2).run(suite)
