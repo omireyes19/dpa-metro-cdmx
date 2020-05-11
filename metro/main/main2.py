c346d058c240627fc9c9abad1c879cc36e27fe4f
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-10 22:39:36 -0500

    corrijo main.py

--- metro/main/main2.py
+++ metro/main/main2.py
@@ -2,7 +2,7 @@ import marbles.core
 from ParametrizedTestCase import ParametrizedTestCase
 from TestOne import TestOne
 
-suite = marbles.TestSuite()
+suite = marbles.core.TestSuite()
 suite.addTest(ParametrizedTestCase.parametrize(TestOne, param=42))
 suite.addTest(ParametrizedTestCase.parametrize(TestOne, param=13))
-marbles.TextTestRunner(verbosity=2).run(suite)
+marbles.core.TextTestRunner(verbosity=2).run(suite)
