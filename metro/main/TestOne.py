fbef5a3b737c23ffaeb9a69ed4154eda9c7064f4
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-10 22:54:54 -0500

    pruebo con mis parametros

--- metro/main/TestOne.py
+++ metro/main/TestOne.py
@@ -6,5 +6,5 @@ class TestMarbles(ParametrizedTestCase):
 
     def test_records_not_empty(self):
         print(self.year)
-        records = call_to_api.get_information(self,2020,1,"Chabacano")
+        records = call_to_api.get_information(self,self.year,self.month,self.station)
         self.assertNotEqual(len(records), 0, note=" the names should be uppercase because bla bla bla")
