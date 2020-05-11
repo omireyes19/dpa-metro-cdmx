50961e93a2a504adfdabe9325a71dc00e960a4c1
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-11 09:33:01 -0500

    agrego return

--- metro/main/translation.py
+++ metro/main/translation.py
@@ -4,3 +4,4 @@ class translation:
     def get_dataframe(self,json_content):
         df = pd.DataFrame(json_content)[["fecha","linea","afluencia"]]
         df.columns = ["date","line","influx"]
+        return df
