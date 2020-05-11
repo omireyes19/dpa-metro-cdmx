52524e50b05128dd4260e0cb7fdd487d8dfd0eb4
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-10 22:52:57 -0500

    pruebo con mis parametros

--- metro/main/TestOne.py
+++ metro/main/TestOne.py
@@ -1,9 +1,10 @@
+from call_to_api import call_to_api
 from ParametrizedTestCase import ParametrizedTestCase
 
-class TestOne(ParametrizedTestCase):
-    def test_something(self):
-        print ('param =', self.param)
-        self.assertEqual(1, 1)
 
-    def test_something_else(self):
-        self.assertEqual(2, 2)
+class TestMarbles(ParametrizedTestCase):
+
+    def test_records_not_empty(self):
+        print(self.year)
+        records = call_to_api.get_information(self,2020,1,"Chabacano")
+        self.assertNotEqual(len(records), 0, note=" the names should be uppercase because bla bla bla")
