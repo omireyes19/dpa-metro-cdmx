9d5bf5aa15d23cf992d8a971740fe0a722c62486
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 14:21:31 -0500

    convierto array a lista

--- metro/train/LabelTest.py
+++ metro/train/LabelTest.py
@@ -6,7 +6,7 @@ class LabelTest(ParametrizedLabelTest):
     def test_label_creation(self):
         intquar_ran = interquartile_range()
         label_df = intquar_ran.create_label(intquar_ran.join_range(self.cleaned_data, intquar_ran.calculate_range(self.cleaned_data)))
-        distinct_labels = label_df['label'].unique()[0]
+        distinct_labels = label_df['label'].unique().tolist()
 
         print("aqui"+str(distinct_labels))
         self.assertListEqual(distinct_labels, range(1, 4))
