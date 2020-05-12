49c88bad4be31fbe68edf97a23abf420bb730cbd
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 04:17:10 -0500

    corrijo if de fallo

--- metro/raw_ingest_unittest.py
+++ metro/raw_ingest_unittest.py
@@ -19,7 +19,7 @@ class raw_unittest_task(luigi.Task):
         test_exit_code = int(not result.wasSuccessful())
 
         if test_exit_code == 1:
-            print(Los datos que cargaste tienen longitud cero)
+            raise Exception("Los datos que cargaste tienen longitud cero :(")
         else:
             with self.output().open('w') as output_file:
                 output_file.write(str(self.today)+","+str(self.year)+","+str(self.month)+","+self.station)
