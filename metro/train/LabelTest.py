4a67ae5f9a9944dd69c87fb0e0712336fbc9be17
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 14:03:06 -0500

    instancio la clase

--- metro/train/LabelTest.py
+++ metro/train/LabelTest.py
@@ -4,6 +4,7 @@ from train.ParametrizedLabelTest import ParametrizedLabelTest
 
 class LabelTest(ParametrizedLabelTest):
     def test_label_creation(self):
-        label_df = interquartile_range.create_label(self, interquartile_range.join_range(self, self.cleaned_data, interquartile_range.calculate_range(self, self.cleaned_data)))
+        intquar_ran = interquartile_range()
+        label_df = intquar_ran.create_label(intquar_ran.join_range(self.cleaned_data, intquar_ran.calculate_range(self.cleaned_data)))
         distinct_labels = label_df['label'].distinct()
         self.assertIn(distinct_labels, range(1, 4))
