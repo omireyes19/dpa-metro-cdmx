6b953e19ccf11db2b22e60a464664f9da8410931
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 15:39:08 -0500

    ordeno la lista

--- metro/train/LabelTest.py
+++ metro/train/LabelTest.py
@@ -6,6 +6,6 @@ class LabelTest(ParametrizedLabelTest):
     def test_label_creation(self):
         intquar_ran = interquartile_range()
         label_df = intquar_ran.create_label(intquar_ran.join_range(self.cleaned_data, intquar_ran.calculate_range(self.cleaned_data)))
-        distinct_labels = label_df['label'].unique().tolist()
+        distinct_labels = label_df['label'].unique().tolist().sort()
 
         self.assertListEqual(distinct_labels, [1, 2, 3])
