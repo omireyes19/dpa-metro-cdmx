a2b78b95767ec44f65fc0739a839cc1303883884
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 17:18:39 -0500

    actualizo pu

--- metro/ingest/DateTest.py
+++ metro/ingest/DateTest.py
@@ -4,6 +4,5 @@ from ingest.ParametrizedDateTest import ParametrizedDateTest
 
 class DateTest(ParametrizedDateTest):
     def test_date_cols_created(self):
-        previous_columns = len(self.csv_data.columns)
         df = date_variables.add_date_variables(self, self.csv_data)
-        self.assertEqual(len(df.columns), previous_columns + 3)
+        self.assertEqual(len(df.columns), 6)
