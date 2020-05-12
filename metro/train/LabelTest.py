346cf522362424b4a8bf887550096b731ee591d9
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 14:06:12 -0500

    cambio distinct por unique

--- metro/train/LabelTest.py
+++ metro/train/LabelTest.py
@@ -6,5 +6,7 @@ class LabelTest(ParametrizedLabelTest):
     def test_label_creation(self):
         intquar_ran = interquartile_range()
         label_df = intquar_ran.create_label(intquar_ran.join_range(self.cleaned_data, intquar_ran.calculate_range(self.cleaned_data)))
-        distinct_labels = label_df['label'].distinct()
+        distinct_labels = label_df['label'].unique()
+
+        print("aqui"+str(distinct_labels))
         self.assertIn(distinct_labels, range(1, 4))
