ffde723a98a3e9f6fe91d9c255ca50c180ac2c5a
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 01:58:28 -0500

    imprimo df

--- metro/train/predictions.py
+++ metro/train/predictions.py
@@ -14,6 +14,8 @@ class predictions:
     def get_predictions(self,spark,df):
         data = spark.createDataFrame(df)
 
+        data.groupBy("day_of_week").count().show()
+
         n = data.count()
 
         data = data.withColumn('line_crossing', col('line_crossing').cast(IntegerType()))
