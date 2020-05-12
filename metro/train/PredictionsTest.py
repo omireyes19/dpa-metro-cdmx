3834b03482d6a97fd42a7a390cb7e77c0cd03dd9
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 14:52:14 -0500

    convierto de index a list

--- metro/train/PredictionsTest.py
+++ metro/train/PredictionsTest.py
@@ -6,7 +6,7 @@ class PredictionsTest(ParametrizedPredictionsTest):
     def test_predictions(self):
         pred = predictions()
         predictions_df = pred.get_predictions(self.spark, self.model_data)
-        column_names_df = predictions_df.columns
+        column_names_df = predictions_df.columns.tolist()
 
         print(column_names_df)
         self.assertTrue(column_names_df.contains('prediction'))
