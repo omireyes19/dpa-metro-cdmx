76cb2b83bb79abed506b47b2e4f945b49524a151
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 15:43:36 -0500

    agrego print

--- metro/train/LabelTest.py
+++ metro/train/LabelTest.py
@@ -6,6 +6,7 @@ class LabelTest(ParametrizedLabelTest):
     def test_label_creation(self):
         intquar_ran = interquartile_range()
         label_df = intquar_ran.create_label(intquar_ran.join_range(self.cleaned_data, intquar_ran.calculate_range(self.cleaned_data)))
-        distinct_labels = label_df['label'].unique().tolist().sort()
+        distinct_labels = label_df['label'].unique().tolist()
+        distinct_labels.sort()
         print("aqui" + str(distinct_labels))
         self.assertListEqual(distinct_labels, [1, 2, 3])
