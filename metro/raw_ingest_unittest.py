71b01d2ef597e0e8ac1203b79e83497363f368c5
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 03:34:28 -0500

    corrijo if de fallo

--- metro/raw_ingest_unittest.py
+++ metro/raw_ingest_unittest.py
@@ -17,7 +17,7 @@ class raw_unittest_task(luigi.Task):
         suite.addTest(ParametrizedCallToAPITest.parametrize(CallToAPITest, year=self.year, month=self.month, station=self.station))
         log = unittest.TextTestRunner(verbosity=2).run(suite)
 
-        if "FAIL" in log:
+        if "FAIL" in str(log):
             print("FAIL")
 
         with self.output().open('w') as output_file:
