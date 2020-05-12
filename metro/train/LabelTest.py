9b068b921b511ad1d1a66e428f63b2c084c07bc9
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-05-12 14:19:31 -0500

    obtengo lista de valores unicos!

--- metro/train/LabelTest.py
+++ metro/train/LabelTest.py
@@ -6,7 +6,7 @@ class LabelTest(ParametrizedLabelTest):
     def test_label_creation(self):
         intquar_ran = interquartile_range()
         label_df = intquar_ran.create_label(intquar_ran.join_range(self.cleaned_data, intquar_ran.calculate_range(self.cleaned_data)))
-        distinct_labels = label_df['label'].unique()
+        distinct_labels = label_df['label'].unique()[0]
 
         print("aqui"+str(distinct_labels))
         self.assertListEqual(distinct_labels, range(1, 4))
