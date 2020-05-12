dcc202ffaa2f3b9bff72c43cf605701f97699532
Rubio Gonzalez Maria Fernanda <MB84276@cb.bbvabancomer.com.mx>
2020-05-11 19:47:05 -0500

    Pruebas unitarias borrador

--- metro/main/ParametrizedTestCase.py
+++ metro/main/ParametrizedTestCase.py
@@ -4,15 +4,20 @@ class ParametrizedTestCase(unittest.TestCase):
     """ TestCase classes that want to be parametrized should
         inherit from this class.
     """
-    def __init__(self, methodName='runTest', year=None, month=None, station=None, raw_json=None):
+    def __init__(self, methodName='runTest', year=None, month=None, station=None, raw_json=None, csv_data=None,
+    cleaned_data=None, model_data=None):
         super(ParametrizedTestCase, self).__init__(methodName)
         self.year = year
         self.month = month
         self.station = station
         self.raw_json = raw_json
+        self.csv_data = csv_data
+        self.cleaned_data = cleaned_data
+        self.model_data = model_data
 
     @staticmethod
-    def parametrize(testcase_klass, year=None, month=None, station=None, raw_json=None):
+    def parametrize(testcase_klass, year=None, month=None, station=None, raw_json=None, csv_data=None,
+    cleaned_data=None, model_data=None):
         """ Create a suite containing all tests taken from the given
             subclass, passing them the parameter 'param'.
         """
@@ -20,5 +25,6 @@ class ParametrizedTestCase(unittest.TestCase):
         testnames = testloader.getTestCaseNames(testcase_klass)
         suite = unittest.TestSuite()
         for name in testnames:
-            suite.addTest(testcase_klass(name, year = year, month = month, station = station, raw_json = raw_json))
+            suite.addTest(testcase_klass(name, year = year, month = month, station = station, raw_json = raw_json,
+             csv_data = csv_data, cleaned_data = cleaned_data, model_data = model_data))
         return suite
--- metro/main/TestOne.py
+++ metro/main/TestOne.py
@@ -1,7 +1,9 @@
 from call_to_api import call_to_api
 from translation import translation
 from ParametrizedTestCase import ParametrizedTestCase
-
+from date_variables import date_variables
+from metro.training.interquartile_range import interquartile_range
+from metro.training.predictions import predictions
 
 class TestOne(ParametrizedTestCase):
     def test_records_not_empty(self):
@@ -12,3 +14,22 @@ class TestOne(ParametrizedTestCase):
         df = translation.get_dataframe(self,self.raw_json)
         number_of_columns = len(df.columns)
         self.assertEqual(number_of_columns, 3)
+
+    def test_date_cols_created(self):
+        previous_columns = len(self.csv_data.columns)
+        df = date_variables.add_date_variables(csv_data)
+        self.assertEqual(len(df.columns), previous_columns + 3)
+
+    def test_label_creation(self):
+        label_df = interquartile_range.create_label(interquartile_range.join_range(self.cleaned_data, interquartile_range.calculate_range(self.cleaned_data)))
+        distinct_labels = label_df['label'].distinct()
+        self.assertIn(distinct_labels, range(1,4)
+
+    def test_influx_values(self):
+        min_influx = min(self.cleaned_data['influx'])
+        self.assertGreaterEqual(min_influx, 0)
+
+    def test_predictions(self):
+        predictions_df = predictions.get_predictions(spark,self.model_data)
+        column_names_df = predictions_df.columns
+        self.assertTrue(column_names_df.contains('prediction')
