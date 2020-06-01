f9e8f6622dd075dfbab2cdd1d8b63a003acadafb
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-06-01 08:30:51 -0500

    cambio lectura de api a todas las estaciones

--- metro/ingest/call_to_api.py
+++ metro/ingest/call_to_api.py
@@ -2,14 +2,14 @@ from calendar import monthrange
 import requests
 
 class call_to_api:
-    def get_information(self, year, month, station):
+    def get_information(self, year, month):
         days_in_month = monthrange(year, month)[1]
 
         records = []
         for day in range(days_in_month):
             fecha = str(year)+"-"+str(month).zfill(2)+"-"+str(day+1).zfill(2)
-            api_url = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=afluencia-diaria-del-metro-cdmx&" \
-                      "sort=-fecha&facet=fecha&facet=linea&facet=estacion&refine.fecha="+fecha+"&refine.estacion="+station
+            api_url = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=afluencia-diaria-del-metro-cdmx" \
+                      "&q=&rows=-1&sort=-fecha&facet=fecha&refine.fecha="+fecha
 
             r = requests.get(url = api_url)
             data = r.json()
