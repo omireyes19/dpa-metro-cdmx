08ddc238ef5ad1403195e5cb9e9c66401013cd63
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-06-01 09:31:26 -0500

    leer todas estaciones unittest

--- metro/ingest/CallToAPITest.py
+++ metro/ingest/CallToAPITest.py
@@ -3,5 +3,5 @@ from ingest.ParametrizedCallToAPITest import ParametrizedCallToAPITest
 
 class CallToAPITest(ParametrizedCallToAPITest):
     def test_records_not_empty(self):
-        records = call_to_api.get_information(self,self.year,self.month,self.station)
+        records = call_to_api.get_information(self,self.year,self.month)
         self.assertNotEqual(len(records), 0)
--- metro/ingest/ParametrizedCallToAPITest.py
+++ metro/ingest/ParametrizedCallToAPITest.py
@@ -4,14 +4,13 @@ class ParametrizedCallToAPITest(unittest.TestCase):
     """ TestCase classes that want to be parametrized should
         inherit from this class.
     """
-    def __init__(self, methodName='runTest', year=None, month=None, station=None):
+    def __init__(self, methodName='runTest', year=None, month=None):
         super(ParametrizedCallToAPITest, self).__init__(methodName)
         self.year = year
         self.month = month
-        self.station = station
 
     @staticmethod
-    def parametrize(testcase_klass, year=None, month=None, station=None):
+    def parametrize(testcase_klass, year=None, month=None):
         """ Create a suite containing all tests taken from the given
             subclass, passing them the parameter 'param'.
         """
@@ -19,5 +18,5 @@ class ParametrizedCallToAPITest(unittest.TestCase):
         testnames = testloader.getTestCaseNames(testcase_klass)
         suite = unittest.TestSuite()
         for name in testnames:
-            suite.addTest(testcase_klass(name, year = year, month = month, station = station))
+            suite.addTest(testcase_klass(name, year = year, month = month))
         return suite
