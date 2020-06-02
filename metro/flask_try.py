ac389a3f9a9f00bec8b48f0b6f2ba9d94d717eaa
omar.reyes.villa <omar.reyes.villa@bbva.com>
2020-06-02 08:07:56 -0500

    agrego json

--- metro/flask_try.py
+++ metro/flask_try.py
@@ -10,14 +10,14 @@ api = Api(app)
 
 @api.route("/date/<string:date>")
 class GetPredictions(Resource):
-    year = date[:4]
-    month = date[4:]
+    def get(self, date):
+        year = date[:4]
+        month = date[4:]
 
-    def get(self):
         ses = boto3.session.Session(profile_name='omar', region_name='us-east-1')
         s3_resource = ses.resource('s3')
 
-        obj = s3_resource.Object("dpa-metro-predictions", "year={}/month={}/{}.csv".format(str(self.year), str(self.month).zfill(2), str(self.year)+str(self.month).zfill(2)))
+        obj = s3_resource.Object("dpa-metro-predictions", "year={}/month={}/{}.csv".format(str(year), str(month).zfill(2), str(year)+str(month).zfill(2)))
 
         file_content = obj.get()['Body'].read().decode('utf-8')
         df = pd.read_csv(StringIO(file_content))
